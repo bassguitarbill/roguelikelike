829b87c0b013a120430a43e7c4de87d354589b20
bassguitarbill <bassguitarbill@gmail.com>
2019-10-20 15:18:38 -0400

    Generate nine random rooms

--- js/Grid.ts
+++ js/Grid.ts
@@ -5,24 +5,65 @@ export class Grid {
   private readonly rowHeight = 8;
   private readonly columnWidth = 8;
 
-  cells : Array<Array<number>> = [];
+  private readonly horizontalRooms = 3;
+  private readonly verticalRooms = 3;
+  private readonly CHANCE_OF_MISSING_ROOM = 0.08;
+  private readonly minimumNumberOfRooms = 6;
+  private readonly minRoomWidth = 2;
+  private readonly maxRoomWidth = Math.floor(this.columns / this.horizontalRooms) - 2;
+  private readonly minRoomHeight = 2;
+  private readonly maxRoomHeight = Math.floor(this.rows / this.verticalRooms) - 2;
+
+  cells: Array<Array<number>> = [];
+  rooms: Array<Array<Room>> = [];
 
   constructor() {
     this.generateMap();
   }
 
   generateMap() {
-    this.forEachCell(() => Math.floor(Math.random() * 2));
+    this.blankMap();
+    this.generateRooms();
   }
 
-  forEachCell(callback : Function) {
-    for(let x=0; x<this.columns; x++) {
-      if(!this.cells[x]) this.cells.push([]);
-      for(let y=0; y<this.rows; y++) {
-        if(!this.cells[x][y]) this.cells[x].push([]);
-        this.cells[x][y] = callback(x,y);
+  blankMap() {
+    for(let x=0; x < this.columns; x++) {
+      this.cells[x] = [];
+      for(let y=0; y < this.rows; y++) {
+        this.cells[x].push(0);
       }
     }
+  }
+
+  generateRooms() {
+    for(let x=0; x<this.horizontalRooms; x++) {
+      this.rooms[x] = [];
+      for(let y=0; y<this.verticalRooms; y++) {
+        this.rooms[x][y] = this.generateRoom(x, y);
+      }
+    }
+  }
+
+  generateRoom(roomGridX: number, roomGridY: number) : Room {
+    const room = { x: 0, y: 0, height: 0, width: 0};
+    this.setRoomSize(room);
+    this.setRoomPosition(room, roomGridX, roomGridY);
+    for(let x=0; x<room.width; x++) {
+      for(let y=0; y<room.height; y++) {
+        this.cells[x + room.x][y + room.y] = 1;
+      }
+    }
+    return room;
+  }
+
+  setRoomSize(room : Room) : void {
+    room.width = Math.floor(Math.random() * (this.maxRoomWidth - this.minRoomWidth)) + this.minRoomWidth;
+    room.height = Math.floor(Math.random() * (this.maxRoomHeight - this.minRoomHeight)) + this.minRoomHeight;
+  }
+
+  setRoomPosition(room: Room, roomGridX: number, roomGridY: number) {
+    room.x = Math.floor(Math.random() * (this.maxRoomWidth - room.width)) + Math.floor(roomGridX * this.columns / this.horizontalRooms);
+    room.y = Math.floor(Math.random() * (this.maxRoomHeight - room.height)) + Math.floor(roomGridY * this.rows / this.verticalRooms);
   }
 
   draw(ctx : CanvasRenderingContext2D) {
@@ -41,3 +82,10 @@ export class Grid {
 }
 
 const cellColors = ['black', 'lightgrey']
+
+interface Room {
+  x: number,
+  y: number,
+  height: number,
+  width: number,
+}
